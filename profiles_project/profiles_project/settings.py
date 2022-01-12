c5aa9cd1cc53595e3be49daecc5880e5eb8c8320
mugdha273 <mugdhasharma0327@gmail.com>
2022-01-12 14:33:55 +0530

    admin set

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
