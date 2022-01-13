9f373d67fc66a4d46af9de9c4ce387bf1b258e41
mugdha273 <mugdhasharma0327@gmail.com>
2022-01-13 17:20:41 +0530

    search filter

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,6 +1,7 @@
+from re import search
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import serializers
 from . import models 
@@ -108,3 +109,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
