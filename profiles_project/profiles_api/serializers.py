d23e35893d185aa73146c9edd4429682e4aad09b
mugdha273 <mugdhasharma0327@gmail.com>
2022-01-13 17:15:35 +0530

    tokenauthentication and permission.py add

--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -1,6 +1,27 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView"""
     name = serializers.CharField(max_length=10)
+    
+class UserProfileSerialier(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+    
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+        
+        return user
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 from profiles_api import serializers
+from . import models 
+from . import permissions
 
 
 class HelloApiView(APIView):
@@ -48,3 +51,60 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+    
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API Viewsets"""
+    serializer_class = serializers.HelloSerializer
+    
+    def list(self, request):
+        """Return a hello message"""
+        
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+    
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
+    
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerialier
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
